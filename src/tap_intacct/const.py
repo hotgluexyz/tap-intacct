4d73d4ad6eba0b171681aa3ca0d210f17303e081
Azmat Ali <mian.azmatali.9@gmail.com>
2025-04-03 08:26:44 -0400

    HGI-7554 Add employees stream

--- src/tap_intacct/const.py
+++ src/tap_intacct/const.py
@@ -36,6 +36,7 @@ KEY_PROPERTIES = {
     'budget_details': ["RECORDNO"],
     'budget_list': ["RECORDNO"],
     'po_documents': ["RECORDNO"],
+    'employees': ["RECORDNO"],
 }
 
 # List of available objects with their internal object-reference/endpoint name.
@@ -69,11 +70,13 @@ INTACCT_OBJECTS = {
     "budget_list": "GLBUDGETHEADER",
     "budget_details": "GLBUDGETITEM",
     "po_documents": "PODOCUMENT",
+    "employees": "EMPLOYEE",
 }
 
 REP_KEYS = {
     "audit_history" : "ACCESSTIME",
-    "budget_details": None
+    "budget_details": None,
+    "employees": None,
 }
 
 IGNORE_FIELDS =["PASSWORD"]
